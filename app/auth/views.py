51c5764103577bc4b20db0aeb5bafe6cf318b136
bigzhao <350490072@qq.com>
2016-08-18 11:07:26 +0800

    对project增加del_flag,类型布尔，默认False，增添dustbin面板

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
-from .forms import LoginForm, RegisterForm, EditForm, ChangePasswdForm
+from .forms import RegisterForm, EditForm, ChangePasswdForm
 from .. import db
 
 
